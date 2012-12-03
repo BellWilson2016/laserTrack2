35b2d04842ad19b6f40f73aa680655fd25916548
joebell <joe.bell@gmail.com>
2012-12-03 17:43:02 -0500

    Implemented calculate by single fly in liveTrack

--- Laser Distributions/laserFlatHalves.m
+++ Laser Distributions/laserFlatHalves.m
@@ -1,17 +1,13 @@
 %%
 % Returns flat laser distributions on each half.
 %
-function returnPower = laserFlatHalves(args)  
+function returnPower = laserFlatHalves(X,Y,args)  
 
-    global trackingParams;   
-    laserPowers = args;
     
-    leftP  = laserPowers(1);
-    rightP = laserPowers(2);
+    leftP  = args(1);
+    rightP = args(2);
     
+    
+    returnPower = (X < 0).*leftP + (X >= 0).*rightP;
 
-    xPos = trackingParams.bodyX + trackingParams.headX;     
-    lp = (xPos < 0).*leftP + (xPos >= 0).*rightP;
 
-
-    returnPower = lp;
