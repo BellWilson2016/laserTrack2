85b79636976a362536985776d6f40241e7b35e35
joebell <joe.bell@gmail.com>
2012-12-05 16:31:57 -0500

    Updated event scheduler

--- scheduleEvent.m
+++ scheduleEvent.m
@@ -5,7 +5,7 @@ function scheduleEvent(timeDelay, cmdToRun);
 	disp(['scheduleEvent() for ',num2str(timeDelay/60),' (min) =>  ',cmdToRun,]);
 
 	nEvents = size(allScheduledEvents,2);
-	allScheduledEvents(end+1) = nEvents + 1;
+	allScheduledEvents{end+1} = cmdToRun;
 	aTimer = timer('ExecutionMode','singleShot',...
 					'StartDelay', timeDelay,...
 					'TimerFcn', {@doLater,cmdToRun,nEvents + 1});
@@ -17,9 +17,10 @@ function doLater(obj,event,cmdToRun,eventN)
 	
 	global allScheduledEvents;
 
-	totalEvents = allScheduledEvents(end);
+	totalEvents = size(allScheduledEvents,2);
 
 	disp(['scheduleEvent() is running event #',num2str(eventN),' of ',num2str(totalEvents)]);
+	disp(['                =>  ',cmdToRun]);
 	eval(cmdToRun);
 
 
