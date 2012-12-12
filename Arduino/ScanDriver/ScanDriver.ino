e0a4e563decddb10a79c591bc2bbc50c81f5fac9
joebell <joe.bell@gmail.com>
2012-12-11 23:59:52 -0500

    Push from RTFW

--- Arduino/ScanDriver/ScanDriver.ino
+++ Arduino/ScanDriver/ScanDriver.ino
@@ -207,7 +207,7 @@ void loop() {
       } else {
         phase = 1;
         queueSerialReturn(0x10 + currentZone, prevTimePoint + nextTimeGap);                  // Denotes laser on.  Do this in advance in case of short laser epochs.
-        queueSerialReturn(0x18 + currentZone, prevTimePoint + nextTimeGap + laserDuration);  // Denotes laser off.
+        // queueSerialReturn(0x18 + currentZone, prevTimePoint + nextTimeGap + laserDuration);  // Denotes laser off.
       }     
       break;
     
