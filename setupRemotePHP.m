73bf2f7bc64a76aaebb6068a899bbaf1aa85ce25
joebell <joe.bell@gmail.com>
2013-01-02 15:43:37 -0500

    Auto-update: 02-Jan-2013 15:43:37

--- setupRemotePHP.m
+++ setupRemotePHP.m
@@ -1,2 +1,8 @@
 function setupRemotePHP()
 
+	if isunix()
+		[status, currentIP] = system('ip addr show eth0 | grep -Po ''(?<=inet )[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*''');
+	else
+		disp('Local webserving not setup in Windows, not pushing PHP script.');
+		return;
+	end
