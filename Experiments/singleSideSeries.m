8b9ebe9f4926d9c7e81473f82aefdbaa40903438
joebell <joe.bell@gmail.com>
2013-01-10 10:05:15 -0500

    Auto-update: 10-Jan-2013 10:05:09

--- Experiments/singleSideSeries.m
+++ Experiments/singleSideSeries.m
@@ -5,8 +5,8 @@ function singleSideSeries()
 
 	% Setup generic experimental info
 	exp.experimentName = [datestr(now,'YYmmDD-HHMMss-'),'singleSideSeries'];
-    exp.genotype       = 'NorpA[7]/y ; ChR2/+ ; ChR2/Or22a-Gal4';
-    exp.flyAge         = 7;    % Days
+    exp.genotype       = 'NorpA[7]/y ; ChR2/Or10a-Gal4 ; ChR2/+';
+    exp.flyAge         = 8;    % Days
     exp.sex            = 'M';
     exp.odor           = 'none';
     exp.odorConc       = 0;          % log10
