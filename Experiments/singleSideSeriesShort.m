93b51b5024ed9dfece7694faf18cc4a191c6a5af
joebell <joe.bell@gmail.com>
2013-01-21 17:47:07 -0500

    Auto-update: 21-Jan-2013 17:47:07

---
+++ Experiments/singleSideSeriesShort.m
@@ -0,0 +1,39 @@
+function singleSideSeries() 
+
+	global allScheduledEvents;
+	allScheduledEvents = [];	% Clear existing schedule
+
+	% Setup generic experimental info
+	exp.experimentName = [datestr(now,'YYmmDD-HHMMss-'),'singleSideSeries'];
+    exp.genotype       = 'NorpA[7]/y ; ChR2/Or42a-Gal4 ; ChR2/+';
+    exp.flyAge         = 8;    % Days
+    exp.sex            = 'M';
+    exp.odor           = 'none';
+    exp.odorConc       = 0;          % log10
+    exp.flowRate       = 300;        % mL/side
+	exp.laserPowers    = [0:8:48];
+	exp.laserFilter    = .25;
+	exp.nReps          = 9;	
+
+	% One pass has each power versus 0 on each side;
+	onePass = [exp.laserPowers , zeros(1,size(exp.laserPowers,2)) ;...
+		 		zeros(1,size(exp.laserPowers,2)) , exp.laserPowers];
+	nSeq = size(exp.laserPowers,2)*2;
+
+	nSched = 0;
+	for repN = 1:exp.nReps
+		% Randomize the presentation order
+		order = randperm(nSeq);
+		for seqN = 1:nSeq
+			powerL = onePass(1,order(seqN));
+			powerR = onePass(2,order(seqN));
+			% Setup the protocol, laser distribution, and arguments
+			exp.protocol	 = @laser_1_2L;
+			exp.protocolArgs = {@laserFlatHalves, [powerL, powerR]};
+			cmd = {@runLaserProtocol,exp};
+			scheduleEvent(5 + (3.5*60)*nSched, cmd);  
+			nSched = nSched + 1;
+		end
+	end
+
+	
