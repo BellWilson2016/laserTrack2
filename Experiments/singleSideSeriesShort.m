ba6de4c023e7f71099402c8bfbdc8871feb02c46
joebell <joe.bell@gmail.com>
2013-02-21 21:44:12 -0500

    Auto-update: 21-Feb-2013 21:44:11

--- Experiments/singleSideSeriesShort.m
+++ Experiments/singleSideSeriesShort.m
@@ -10,10 +10,10 @@ function singleSideSeriesShort()
     exp.sex            = 'M';
     exp.odor           = 'none';
     exp.odorConc       = 0;          % log10
-    exp.flowRate       = 1200;        % mL/side
+    exp.flowRate       = 0;        % mL/side
 	exp.laserPowers    = [4,6,9,14,21,32,48,72];
 	exp.laserFilter    = .25;
-	exp.nReps          = 8;	
+	exp.nReps          = 12;	
 
 	% One pass has each power versus 0 on each side;
 	onePass = [exp.laserPowers , zeros(1,size(exp.laserPowers,2)) ;...
