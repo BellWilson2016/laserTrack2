2f2b6adafe7ae808437d8ec0fb7fa92ec50c653e
joebell <joe.bell@gmail.com>
2013-04-30 11:09:55 -0400

    Auto-update: 30-Apr-2013 11:09:55

---
+++ Experiments/singleSideFreqSeries.m
@@ -0,0 +1,42 @@
+function singleSideFreqSeries() 
+
+	global allScheduledEvents;
+	allScheduledEvents = [];	% Clear existing schedule
+
+	% Setup generic experimental info
+	exp.experimentName = [datestr(now,'YYmmDD-HHMMss-'),'singleSideFreqSeries'];
+    exp.genotype       = 'NorpA[7]/y ; ChR2/Or42b-Gal4 ; ChR2/+';
+    exp.flyAge         = 5;    % Days
+    exp.sex            = 'M';
+    exp.odor           = 'none';
+    exp.odorConc       = 0;          % log10
+    exp.flowRate       = 1200;       % mL/side
+	%exp.laserPowers    = [4,6,9,14,21,32,48,72];
+	exp.laserPowers    = [0,56,156,206,236,246,251,254];
+	%exp.laserPowers    = [108,162];
+	exp.laserFilter    = 1;
+	exp.nReps          = 8;
+	exp.comment		   = 'Freq. series';	
+
+	% One pass has each power versus 0 on each side;
+	onePass = [exp.laserPowers , zeros(1,size(exp.laserPowers,2)) ;...
+		 		zeros(1,size(exp.laserPowers,2)) , exp.laserPowers];
+	nSeq = size(exp.laserPowers,2)*2;
+
+	nSched = 0;
+	for repN = 1:exp.nReps
+		% Randomize the presentation order
+		order = randperm(nSeq);
+		for seqN = 1:nSeq
+			powerL = onePass(1,order(seqN));
+			powerR = onePass(2,order(seqN));
+			% Setup the protocol, laser distribution, and arguments
+			exp.protocol	 = @laser_1_halfL_1;
+			exp.protocolArgs = {@laserFlatHalves, [powerL, powerR]};
+			cmd = {@runLaserProtocol,exp};
+			scheduleEvent(5 + (3.5*60)*nSched, cmd);  
+			nSched = nSched + 1;
+		end
+	end
+
+	
