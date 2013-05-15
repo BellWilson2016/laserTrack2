7facf317e041b4cc961f663f8e0d179070e46902
joebell <joe.bell@gmail.com>
2013-05-15 08:57:41 -0400

    Auto-update: 15-May-2013 08:57:41

--- Laser Distributions/laserLinearFreqGradientY.m
+++ Laser Distributions/laserLinearFreqGradientY.m
@@ -1,12 +1,12 @@
-function returnPower = laserLinearFreqGradient(args)  
+function returnPower = laserLinearFreqGradientY(args)  
 
 	global trackingParams;
 
     leftF  = args(1);
     rightF = args(2);
-	Fgrad = (rightF - leftF)/50;  		% Power units / mm
+	Fgrad = (rightF - leftF)/39;  		% Power units / mm
 
-	returnF = leftF + (trackingParams.bodyX + trackingParams.headX + 25).*Fgrad;    
+	returnF = leftF + (trackingParams.bodyY + trackingParams.headY + 39/2).*Fgrad;    
 	
 	ix = find(returnF < 0);
 	returnF(ix) = 0;
