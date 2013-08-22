f7da3833048209d70968ae27a5122128fc01e3d8
joebell <joe.bell@gmail.com>
2013-08-22 10:05:29 -0400

    Auto-update: 22-Aug-2013 10:05:29

--- Experiments/contingentMiesenbock.m
+++ Experiments/contingentMiesenbock.m
@@ -6,12 +6,12 @@ function contingentMiesenbock()
 	% Setup generic experimental info
 	exp.experimentName = [datestr(now,'YYmmDD-HHMMss-'),'contingentMisenbock'];
     exp.genotype       = 'NorpA[7]/y ; H134R / Gr21a-Gal4 (Suh) ; + / +';
-    exp.flyAge         = 5;    % Days
+    exp.flyAge         = 7;    % Days
     exp.sex            = 'M';
     exp.odor           = 'none';
     exp.odorConc       = 0;          % log10
     exp.flowRate       = 1200;        % mL/side
-	exp.laserPowers    = [90,0];
+	exp.laserPowers    = [240,0];
 	exp.laserFilter    = 1;
 	exp.nReps          = 8;	
 
