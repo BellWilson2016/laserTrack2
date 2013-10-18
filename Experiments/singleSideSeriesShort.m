b327e9b0385a5f317018234b745a6a26d7e2ee35
joebell <joe.bell@gmail.com>
2013-10-18 12:29:20 -0400

    Auto-update: 18-Oct-2013 12:29:20

--- Experiments/singleSideSeriesShort.m
+++ Experiments/singleSideSeriesShort.m
@@ -5,16 +5,16 @@ function singleSideSeriesShort()
 
 	% Setup generic experimental info
 	exp.experimentName = [datestr(now,'YYmmDD-HHMMss-'),'singleSideSeriesShort'];
-    exp.genotype       = 'NorpA[7]/y ; H134R / + ; Or92a-Gal4 / +';
-    exp.flyAge         = 6;    % Days
+    exp.genotype       = 'NorpA[7]/y ; H134R / Or42a-Gal4 ; + / +';
+    exp.flyAge         = 7;    % Days
     exp.sex            = 'M';
     exp.odor           = 'none';
     exp.odorConc       = 0;          % log10
     exp.flowRate       = 1200;       % mL/side
 	%exp.laserPowers    = [2,3,5,7,11,16,24,36];
 	%exp.laserPowers    = [4,6,9,14,21,32,48,72]*2;
-	exp.laserPowers    = [0,4,6,9,14,21,32,48];
-	%exp.laserPowers    = [0];
+	%exp.laserPowers    = [0,4,6,9,14,21,32,48];
+	exp.laserPowers    = [21];
 	%exp.laserPowers = [0, 9, 14, 21, 32, 48, 64, 96];
 	%exp.laserPowers    = [108,162];
 	exp.laserFilter    = 1;
