2c8d54a681c5fd676bf569402b7b9add84f3acc4
Joe Bell <joe.bell@gmail.com>
2014-11-14 09:38:26 -0500

    Auto-update: 14-Nov-2014 09:38:26

--- Experiments/singleSideSeriesShortRB.m
+++ Experiments/singleSideSeriesShortRB.m
@@ -8,11 +8,11 @@ function singleSideSeriesShortRB()
 	% Setup generic experimental info
 	exp.experimentName = [datestr(now,'YYmmDD-HHMMss-'),'singleSideSeriesShortRB'];
     exp.genotype       = 'NorpA[7]/y ; H134R / Or83b-Gal4 ; + / +';
-    exp.flyAge         = 8;    % Days
+    exp.flyAge         = 6;    % Days
     exp.sex            = 'M';
     exp.odor           = 'none';
     exp.odorConc       = 0;          % log10
-    exp.flowRate       = 0;          % mL/side
+    exp.flowRate       = 1200;          % mL/side
     exp.refSide        = [];		 % 1 is left, -1 is right
 	exp.laserPowers    =  [0,2,4,8,12,16,32,64];
 	exp.redMultiplier  = 2.5;
